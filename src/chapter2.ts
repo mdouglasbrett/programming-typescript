622080730670b82b95ca29c64de006c81307da1e
Michael Brett <mdouglasbrett@icloud.com>
2020-08-19 08:24:05 +0100

    Add chapter 2 exercises

---
+++ src/chapter2.ts
@@ -0,0 +1,13 @@
+let a = 1 + 2
+let b = a + 3
+let c = {
+    apple: a,
+    banana: b
+}
+
+// TS can't infer args
+const growApples = (growthFactor: number) => {
+    return c.apple * growthFactor
+}
+
+let d = growApples(4)
