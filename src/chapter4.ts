1d58c7c58c2eddb75fe6ee6a62d819b799a64cb3
Michael Brett <mdouglasbrett@icloud.com>
2020-08-22 18:23:38 +0100

    Add evac case to reserve function

--- src/chapter4.ts
+++ src/chapter4.ts
@@ -8,4 +8,31 @@ const mySumFunc = (a: number, b: number) => {
 
 const mySumFuncWithRest = (...numbers: number[]) => numbers.reduce((total, n) => total + n, 0)
 
+type Reservation = {
+    type: "single" | "return" | "evac"
+} | undefined
+type Reserve = {
+    (from: Date, to: Date, destination: string): Reservation
+    (from: Date, destination: string): Reservation
+    (destination: string): Reservation
+}
+
+let Reserve: Reserve = (fromOrDestination: Date | string, toOrDestination?: Date | string, destination?: string): Reservation => {
+    if (typeof fromOrDestination === "string") {
+        return {
+            type: "evac"
+        }
+    }
+    if (toOrDestination && typeof toOrDestination === 'string') {
+        return {
+            type: "single"
+        }
+    }
+    if (toOrDestination && toOrDestination instanceof Date && destination !== undefined) {
+        return {
+            type: "return"
+        }
+    }
+}
+
 export {}
