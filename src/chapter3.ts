6edfa1b926d5c6a66a731870a8f7e2c141c47a84
Michael Brett <mdouglasbrett@icloud.com>
2020-08-22 17:42:26 +0100

    Add chapter 3 exercises

--- src/chapter3.ts
+++ src/chapter3.ts
@@ -7,4 +7,12 @@ let f = [1, false]
 const g = [3]
 let h = null
 
+let i: 3 = 3
+// i = 4
+let j = [1,2,3]
+j.push(4)
+// j.push('5')
+// let k: never = 4
+let l: unknown = 4
+// let m = l * 2
 export {}
