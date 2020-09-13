6e77fa2129cbe4c928adb6ba08f56f24d69d1e8c
Michael Brett <mdouglasbrett@icloud.com>
2020-09-13 13:27:34 +0100

    Reformat all the files with prettier
    I think I am just going to have to install prettier as step 1 whenever I start a repo from now on

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -1,16 +1,16 @@
-let a = 1 + 2
-let b = a + 3
+let a = 1 + 2;
+let b = a + 3;
 let c = {
-    apple: a,
-    banana: b
-}
+  apple: a,
+  banana: b,
+};
 
 // TS can't infer args
 const growApples = (growthFactor: number) => {
-    return c.apple * growthFactor
-}
+  return c.apple * growthFactor;
+};
 
-let d = growApples(4)
+let d = growApples(4);
 
 // Module vs script behaviour
-export {}
+export {};
--- src/chapter4.ts
+++ src/chapter4.ts
@@ -1,38 +1,53 @@
 // TS can't infer parameters, but will infer a return value
 const mySumFunc = (a: number, b: number) => {
-    return a + b;
-}
+  return a + b;
+};
 
 // Arguments is unsafe, will force n and total to be any
 // const mySumFuncWithArgs = () => Array.from(arguments).reduce((total, n) => total + n, 0)
 
-const mySumFuncWithRest = (...numbers: number[]) => numbers.reduce((total, n) => total + n, 0)
+const mySumFuncWithRest = (...numbers: number[]) =>
+  numbers.reduce((total, n) => total + n, 0);
 
-type Reservation = {
-    type: "single" | "return" | "evac"
-} | undefined
+type Reservation =
+  | {
+      type: "single" | "return" | "evac";
+    }
+  | undefined;
 type Reserve = {
-    (from: Date, to: Date, destination: string): Reservation
-    (from: Date, destination: string): Reservation
-    (destination: string): Reservation
+  (from: Date, to: Date, destination: string): Reservation;
+  (from: Date, destination: string): Reservation;
+  (destination: string): Reservation;
+};
+
+const Reserve: Reserve = (
+  fromOrDestination: Date | string,
+  toOrDestination?: Date | string,
+  destination?: string
+): Reservation => {
+  if (typeof fromOrDestination === "string") {
+    return {
+      type: "evac",
+    };
+  }
+  if (toOrDestination && typeof toOrDestination === "string") {
+    return {
+      type: "single",
+    };
+  }
+  if (
+    toOrDestination &&
+    toOrDestination instanceof Date &&
+    destination !== undefined
+  ) {
+    return {
+      type: "return",
+    };
+  }
+};
+
+function call(): unknown {
+  return {};
 }
 
-const Reserve: Reserve = (fromOrDestination: Date | string, toOrDestination?: Date | string, destination?: string): Reservation => {
-    if (typeof fromOrDestination === "string") {
-        return {
-            type: "evac"
-        }
-    }
-    if (toOrDestination && typeof toOrDestination === 'string') {
-        return {
-            type: "single"
-        }
-    }
-    if (toOrDestination && toOrDestination instanceof Date && destination !== undefined) {
-        return {
-            type: "return"
-        }
-    }
-}
-
-export {}
+export {};
